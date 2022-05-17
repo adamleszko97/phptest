971c2b3b3ba40e153494a42eaf78f7b98de41e30
adamleszko97 <105678499+adamleszko97@users.noreply.github.com>
2022-05-17 15:36:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ if (mysqli_connect_errno())
 {
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
-    //Run the Select query
+   //Run the Select query
 printf("Reading data from table: \n");
 $res = mysqli_query($conn, 'SELECT * FROM test');
 while ($row = mysqli_fetch_assoc($res))
