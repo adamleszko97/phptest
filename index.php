10a57fece5c87ebf1727d235ac98d70564f0d980
adamleszko97 <105678499+adamleszko97@users.noreply.github.com>
2022-05-17 14:15:01 +0200

    Update index.php

--- index.php
+++ index.php
@@ -28,11 +28,16 @@ if (mysqli_connect_errno())
     
     //Run the Select query
 printf("Reading data from table: \n");
+echo "<br>"
 $res = mysqli_query($conn, 'SELECT name, surname FROM credentials');
-while ($row = mysqli_fetch_assoc($res))
- {
-    var_dump($row);
- }
+$result = $conn->query($res);    
+if ($result->num_rows > 0){
+    while ($row = $result -> mysqli_fetch_assoc()){
+        echo "Name: " . $row["name"]. " " . $row["surname"]. "<br>";
+}
+} else {
+  echo "0 results";
+}        
 
 ?>
 </body>
