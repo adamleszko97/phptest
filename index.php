6e2c83f29a4ed7aeea6e8e09113f1be7a2c4141f
adamleszko97 <105678499+adamleszko97@users.noreply.github.com>
2022-05-17 20:37:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -19,13 +19,32 @@ include('config/db_connect.php');
 		<th>Phone number</th>
 		<th>Department</th>
 	</tr>
+	<tr>
+		<th><form class="example" action="action_page.php">
+  <input type="text" placeholder="Search.." name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form></th>
+		<th><form class="example" action="action_page.php">
+  <input type="text" placeholder="Search.." name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form></th>
+		<th><form class="example" action="action_page.php">
+  <input type="text" placeholder="Search.." name="search">
+  <button type="submit"><i class="fa fa-search"></i></button>
+</form></th>
+		<th><select name="cars" id="cars">
+  <option value="SFO">SFO</option>
+  <option value="CHANGE">CHANGE</option>
+  <option value="Internal IT">Internal IT</option>
+</select></th>
+	</tr>
 	<?php
 
 	$sql = "SELECT id, name, department, phone_number FROM employees1";
 	$result = mysqli_query($conn, $sql);   
 	if ($result-> num_rows >0){
 		while ($row = $result-> fetch_assoc()){
-			echo "<tr><td>" . $row["key"] . "</td><td>" . $row["name"] . "</td><td>" . $row["phone_number"] . "</td><td>" . $row["department"] . "</td></tr>";
+			echo "<tr><td>" . $row["id"] . "</td><td>" . $row["name"] . "</td><td>" . $row["phone_number"] . "</td><td>" . $row["department"] . "</td></tr>";
 
 		}
 	echo "</table>";
