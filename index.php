6df85b8097f5a2b32c8aecc07922e8a436d3a314
adamleszko97 <105678499+adamleszko97@users.noreply.github.com>
2022-05-17 20:58:27 +0200

    Update index.php

--- index.php
+++ index.php
@@ -10,8 +10,8 @@ include('config/db_connect.php');
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-<h1>Database search</h1>
 <center>
+<h1>Database search</h1>
 <table>
 	<tr>
 		<th>ID</th>
