93dc9270c5a96238dee09b339e31a1e47efa5c5c
adamleszko97 <105678499+adamleszko97@users.noreply.github.com>
2022-05-17 13:30:33 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
-<?php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP - Hello, World!</title>
+</head>
+<body>
+        <h1><?php echo 'Hello, World!'; ?></h1>
+    <?php
 $host = 'mysqlserverale.mysql.database.azure.com';
 $username = 'ale@mysqlserverale';
 $password = 'Tornado97.';
@@ -18,14 +28,5 @@ if (mysqli_connect_errno())
     die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP - Hello, World!</title>
-</head>
-<body>
-        <h1><?php echo 'Hello, World!'; ?></h1>
 </body>
 </html>
