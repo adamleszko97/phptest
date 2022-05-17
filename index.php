9b673c81362202f3d3f2baafc71cbb46e186f2b8
adamleszko97 <105678499+adamleszko97@users.noreply.github.com>
2022-05-17 18:01:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
  <?php
 
-include('config/db_connect.php');
+$host = 'mysqlserverale.mysql.database.azure.com';
+$username = 'ale@mysqlserverale';
+$password = 'Test12345!';
+$db_name = 'test';
+
+//Initializes MySQLi
+$conn = mysqli_init();
+
+// Establish the connection
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 
 $sql = 'SELECT name, surname FROM credentials';
 $result = mysqli_query($conn, $sql);   
