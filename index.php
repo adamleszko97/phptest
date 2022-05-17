dff7118791f98f3356d52bb2e2866dced3d3d205
adamleszko97 <105678499+adamleszko97@users.noreply.github.com>
2022-05-17 13:23:31 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+$host = 'mysqlserverale.mysql.database.azure.com';
+$username = 'ale@mysqlserverale';
+$password = 'Tornado97.';
+$db_name = 'mysqlserverale';
+
+//Initializes MySQLi
+$conn = mysqli_init();
+
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/DigiCertGlobalRootG2.crt.pem", NULL, NULL);
+
+// Establish the connection
+mysqli_real_connect($conn, 'mysqlserverale.mysql.database.azure.com', 'ale@mysqlserverale', 'Tornado97.', 'mysqlserverale', 3306, NULL, MYSQLI_CLIENT_SSL);
+
+//If connection failed, show the error
+if (mysqli_connect_errno())
+{
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
